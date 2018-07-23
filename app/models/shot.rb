f1a995fa8ec2e1d2cfb1ea869a183e2c70e76837
vita <dragic.hsieh@gmail.com>
2018-07-23 20:13:51 +0800

    Done add shot model validates and modify css style

--- app/models/shot.rb
+++ app/models/shot.rb
@@ -1,3 +1,6 @@
 class Shot < ApplicationRecord
 	belongs_to :user
+	validates :title, presence: true, length: {minimum: 5}
+	validates :description, presence: true
+	validates :link, presence: true
 end
