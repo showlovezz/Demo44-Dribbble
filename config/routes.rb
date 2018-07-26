cc52bfbab3960f9568fd5f9025a68bc83207807f
vita <dragic.hsieh@gmail.com>
2018-07-26 19:05:05 +0800

    Done setup comment resources for public users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :shots
+
+  resources :shots do
+  	resources :comments, only: [:create, :destroy]
+  end
+
   root "shots#index"
 end
