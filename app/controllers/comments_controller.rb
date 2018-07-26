ad538070e47a06897b53f3b319e72f3b9cef13c6
vita <dragic.hsieh@gmail.com>
2018-07-27 00:03:25 +0800

    Done i can delete comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,18 @@
 class CommentsController < ApplicationController
 	
 	before_action :authenticate_user!, only: [:create, :destroy]
+	before_action :find_comment, only: [:create, :destroy]
 
 	def create
-		@shot = Shot.find(params[:shot_id]) # finds the shot with the associated shot_id
 		@comment = @shot.comments.build(comment_params) # creates the comment on the shot passing in params
 		@comment.user_id = current_user.id if current_user # assigns logged in user's ID to comment
 		@comment.save!
+		redirect_to shot_path(@shot)
+	end
+
+	def destroy
+		@comment = Comment.find(params[:id])
+		@comment.destroy
 		redirect_to shot_path(@shot)
 	end
 
@@ -14,6 +20,10 @@ class CommentsController < ApplicationController
 
 	def comment_params
 		params.require(:comment).permit(:content)
+	end
+
+	def find_comment
+		@shot = Shot.find(params[:shot_id]) # finds the shot with the associated shot_id
 	end
 
 end
