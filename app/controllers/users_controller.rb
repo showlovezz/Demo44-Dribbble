dd93e2d1cb471d918241505d7f0b8bf21fcf14f3
vita <dragic.hsieh@gmail.com>
2018-07-29 00:39:19 +0800

    Done user can update the profile details

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,19 @@
 class UsersController < ApplicationController
 
-	before_action :find_user, only: [:show]
+	before_action :find_user, only: [:show, :edit, :update]
 
 	def show
+	end
+
+	def edit
+		unless @user == current_user
+			redirect_to user_path(@user)
+		end
+	end
+
+	def update
+		@user.update(user_params)
+		redirect_to user_path(@user)
 	end
 
 	private
@@ -12,6 +23,10 @@ class UsersController < ApplicationController
 		if @user.blank?
 			redirect_to root_path
 		end
+	end
+
+	def user_params
+		params.require(:user).permit(:name, :intro)
 	end
 
 end
