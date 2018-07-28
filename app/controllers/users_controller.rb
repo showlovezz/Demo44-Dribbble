b0c6fa283f07196ee5c7e966419321be93cbe55f
vita <dragic.hsieh@gmail.com>
2018-07-29 00:20:48 +0800

    Done add user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,17 @@
 class UsersController < ApplicationController
+
+	before_action :find_user, only: [:show]
+
+	def show
+	end
+
+	private
+
+	def find_user
+		@user = User.find_by(id: params[:id])
+		if @user.blank?
+			redirect_to root_path
+		end
+	end
+
 end
